0cd58938f07aeeaaee2341445c221a41e06e8fd6
Bernardo Pedro <be.felix@gmail.com>
2025-02-24 19:55:34 +0000

    algumas coisas adicionadas ao client

--- src/meta1/RMIClient.java
+++ src/meta1/RMIClient.java
@@ -1,18 +1,65 @@
 package meta1;
 
+import java.util.Scanner;
+
 public class RMIClient {
 
  
 
-    private void menu() {
+    private void printmenu() {
+        System.out.println("--------Menu--------");
+        System.out.println("1) Index URL");
+        System.out.println("2) Search for terms");
+        System.out.println("3) List pages linked to a specific page");
+        System.out.println("4) Administration page");
+        System.out.println("5) Exit");
+        System.out.println("--------------------")
+        System.out.println("Choose an option:")
+
     }
 
-    
+    private static isInt(Scanner sc){
+        while(!sc.hasNextInt()){
+            System.out.println("It must be an integer!");
+            sc.next();
+        }
+        int opt = sc.nextInt();
+        return opt;
+    }
+
+        
+
     public static void main(String[] args) {
     
     }
 }
 
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
 
     
 
