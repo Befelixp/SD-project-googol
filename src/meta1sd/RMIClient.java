5ad12e9b6153a0743beb49dc5c29af7bf65f4035
Bernardo Pedro <be.felix@gmail.com>
2025-03-05 15:28:25 +0000

    Já da pra indexar url

--- src/meta1sd/RMIClient.java
+++ src/meta1sd/RMIClient.java
@@ -10,6 +10,7 @@ import java.util.Properties;
 import java.util.Scanner;
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
+import java.io.IOException;
 
 public class RMIClient {
     private Scanner sc = new Scanner(System.in);
@@ -27,8 +28,35 @@ public class RMIClient {
         System.out.println("Choose an option:");
     }
 
-    public void opt(GatewayClientInterface gateway, int option) throws RemoteException {
-
+    // Adicionado InterruptedException à declaração do método
+    public void opt(GatewayClientInterface gateway, int option) throws RemoteException, InterruptedException {
+        switch (option) {
+            case 1:
+                System.out.println("Enter the URL to index:");
+                String url = sc.nextLine();
+                gateway.clientIndexUrl(url);
+                break;
+            case 2:
+                System.out.println("Enter search terms (separated by spaces):");
+                String terms = sc.nextLine();
+                // Assumindo que existe um método para buscar termos
+                // gateway.searchTerms(terms);
+                break;
+            case 3:
+                System.out.println("Enter the URL to find linked pages:");
+                String pageUrl = sc.nextLine();
+                // Assumindo que existe um método para listar páginas vinculadas
+                // gateway.getLinkedPages(pageUrl);
+                break;
+            case 4:
+                System.out.println("Administration page - functionality not implemented");
+                break;
+            case 5:
+                System.out.println("Exiting...");
+                break;
+            default:
+                System.out.println("Invalid option!");
+        }
     }
 
     private static int isIntger(Scanner sc) {
@@ -40,24 +68,54 @@ public class RMIClient {
         return opt;
     }
 
+    private void initclient() {
+        int option = 0;
+
+        while (option != 5) {
+            printmenu();
+            option = isIntger(sc);
+            sc.nextLine(); // Consumir a nova linha após ler o inteiro
+            try {
+                opt(this.gateway, option);
+            } catch (RemoteException e) {
+                System.out.println("Error communicating with server: " + e.getMessage());
+            } catch (InterruptedException e) { // Adicionado tratamento para InterruptedException
+                System.out.println("Operation was interrupted: " + e.getMessage());
+                Thread.currentThread().interrupt(); // Preserva o status de interrupção
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String registryN;
         try {
+            // Verificar argumentos
+            if (args.length < 2) {
+                System.out.println("Usage: java meta1sd.RMIClient <clientID> <propertiesFile>");
+                System.exit(1);
+            }
+
             RMIClient client = new RMIClient();
             client.id = Integer.parseInt(args[0]);
 
+            // Carregar propriedades
             Properties prop = new Properties();
             InputStream input = new FileInputStream(args[1]);
             prop.load(input);
+            input.close();
 
-            registryN = prop.getProperty("registryN");
+            String registryN = prop.getProperty("registryN");
             client.characterLimit = Integer.parseInt(prop.getProperty("limChar"));
 
+            // Conectar ao gateway
             client.gateway = (GatewayClientInterface) Naming.lookup(registryN);
+            System.out.println("Connected to gateway: " + registryN);
+
+            // Iniciar o cliente
+            client.initclient();
 
         } catch (Exception e) {
-            System.out.println("Gateway Unreachable");
+            System.out.println("Gateway Unreachable: " + e.getMessage());
+            e.printStackTrace();
         }
-
     }
 }
